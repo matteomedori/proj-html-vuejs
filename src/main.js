fd3cbf194c825bba3a24ac1423f8176f4ff610f8
matteomedori <matteo.medori.97@gmail.com>
2023-12-19 11:44:19 +0100

    feat: sass installed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 
 // entry point css
-import "./assets/css/style.css";
+import "./assets/css/style.scss";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
